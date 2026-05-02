71dae47742be36ce518039a5bbdd93fa4a668fcd
Shivam Raj <195776198+Shivambhatia-debug@users.noreply.github.com>
2026-05-02 16:32:52 +0530

    backend

--- api/index.py
+++ api/index.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 # Add the project root to the path so we can import from the backend folder
-path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(path, "..", "backend"))
+# Vercel's current working directory is the root of the project
+sys.path.append(os.path.join(os.getcwd(), "backend"))
 
 from app.main import app
+
+# This is for Vercel to handle the app correctly
+app.root_path = "/_backend"
