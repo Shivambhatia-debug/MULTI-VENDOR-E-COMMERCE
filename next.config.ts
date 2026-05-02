5f32e26cb5100379597a522bbe10bf4ad123b1fd
Shivam Raj <195776198+Shivambhatia-debug@users.noreply.github.com>
2026-05-02 13:37:05 +0530

    Backend fixed

--- next.config.ts
+++ next.config.ts
@@ -10,19 +10,6 @@ const nextConfig: NextConfig = {
     ],
   },
   trailingSlash: true,
-  async rewrites() {
-    const isProduction = process.env.NODE_ENV === 'production';
-    const backendUrl = isProduction 
-      ? '/_backend' 
-      : 'http://127.0.0.1:8000';
-
-    return [
-      {
-        source: '/api/python/:path*',
-        destination: `${backendUrl}/api/:path*`,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
